678fe3b5a85b6c1aafe9ab1bea52edffe564d5a5
Philipp Czora <philipp@czora.dev>
2025-01-10 18:35:42 +0100

    Fix import paths, part 3

--- GPT_SoVITS/AR/models/t2s_lightning_module.py
+++ GPT_SoVITS/AR/models/t2s_lightning_module.py
@@ -8,9 +8,9 @@ from typing import Dict
 
 import torch
 from pytorch_lightning import LightningModule
-from ..AR.models.t2s_model import Text2SemanticDecoder
-from ..AR.modules.lr_schedulers import WarmupCosineLRSchedule
-from ..AR.modules.optim import ScaledAdam
+from t2s_model import Text2SemanticDecoder
+from ..modules.lr_schedulers import WarmupCosineLRSchedule
+from ..modules.optim import ScaledAdam
 
 class Text2SemanticLightningModule(LightningModule):
     def __init__(self, config, output_dir, is_train=True):
