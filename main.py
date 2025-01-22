a9dbbbe44d41580e0956b2c07b7d07eaa9a632ff
Philipp Czora <philipp@czora.dev>
2025-01-22 20:13:15 +0100

    Enable model config via env vars

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
+import subprocess
 from functools import wraps
 from io import BytesIO
 
 import flask.wrappers
+import numpy as np
+import soundfile as sf
 from flask import Flask, make_response, request, jsonify
 from werkzeug.utils import secure_filename
 
 from GPT_SoVITS.TTS_infer_pack.TTS import TTS, TTS_Config
-
-from api_v2 import pack_audio
 
 UPLOAD_FOLDER = '/workspace/reference_voices'
 ALLOWED_EXTENSIONS = {'wav', 'txt'}
@@ -17,9 +18,28 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config.from_prefixed_env('GPTSOVITS_API')
 
-config_path = "GPT_SoVITS/configs/tts_infer.yaml"
-tts_config = TTS_Config(config_path)
-tts_pipeline = TTS(tts_config)
+
+
+tts_pipeline = None
+
+
+def init_config() -> TTS_Config:
+    # config_path = "GPT_SoVITS/configs/tts_infer.yaml"
+    # conf = TTS_Config(config_path)
+    conf = TTS_Config()
+    if "BERT_BASE_PATH" in app.config:
+        conf.bert_base_path = app.config["BERT_BASE_PATH"]
+    if "CNHUBERT_BASE_PATH" in app.config:
+        conf.cnhuhbert_base_path = app.config["CNHUBERT_BASE_PATH"]
+    if "DEVICE" in app.config:
+        conf.device = app.config["DEVICE"]
+    if "T2S_WEIGHT_PATH" in app.config:
+        conf.t2s_weights_path = app.config["T2S_WEIGHT_PATH"]
+    if "VERSION" in app.config:
+        conf.version = app.config["VERSION"]
+
+    print(conf)
+    return conf
 
 
 defaults = {
@@ -162,5 +182,51 @@ def from_request_or_default(key: str, req: flask.wrappers.Request):
     else:
         return None
 
+def pack_audio(io_buffer: BytesIO, data: np.ndarray, rate: int, media_type: str):
+    if media_type == "ogg":
+        io_buffer = pack_ogg(io_buffer, data, rate)
+    elif media_type == "aac":
+        io_buffer = pack_aac(io_buffer, data, rate)
+    elif media_type == "wav":
+        io_buffer = pack_wav(io_buffer, data, rate)
+    else:
+        io_buffer = pack_raw(io_buffer, data, rate)
+    io_buffer.seek(0)
+    return io_buffer
+
+def pack_ogg(io_buffer: BytesIO, data: np.ndarray, rate: int):
+    with sf.SoundFile(io_buffer, mode='w', samplerate=rate, channels=1, format='ogg') as audio_file:
+        audio_file.write(data)
+    return io_buffer
+
+
+def pack_raw(io_buffer: BytesIO, data: np.ndarray, rate: int):
+    io_buffer.write(data.tobytes())
+    return io_buffer
+
+
+def pack_aac(io_buffer: BytesIO, data: np.ndarray, rate: int):
+    process = subprocess.Popen([
+        'ffmpeg',
+        '-f', 's16le',  # 输入16位有符号小端整数PCM
+        '-ar', str(rate),  # 设置采样率
+        '-ac', '1',  # 单声道
+        '-i', 'pipe:0',  # 从管道读取输入
+        '-c:a', 'aac',  # 音频编码器为AAC
+        '-b:a', '192k',  # 比特率
+        '-vn',  # 不包含视频
+        '-f', 'adts',  # 输出AAC数据流格式
+        'pipe:1'  # 将输出写入管道
+    ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, _ = process.communicate(input=data.tobytes())
+    io_buffer.write(out)
+    return io_buffer
+
+def pack_wav(io_buffer: BytesIO, data: np.ndarray, rate: int):
+    io_buffer = BytesIO()
+    sf.write(io_buffer, data, rate, format='wav')
+    return io_buffer
 if __name__ == '__main__':
+    tts_config = init_config()
+    tts_pipeline = TTS(tts_config)
     app.run(host='0.0.0.0',port=5555,  debug=True)
